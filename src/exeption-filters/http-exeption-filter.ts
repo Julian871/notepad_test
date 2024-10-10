8554ab542ef1ac9f82f3e7848477d43d91517482
Julian871 <vindiesel.1999@mail.ru>
2024-10-10 09:08:43 +0300

    fix: exception-filter

--- src/exeption-filters/http-exeption-filter.ts
+++ src/exeption-filters/http-exeption-filter.ts
@@ -15,41 +15,10 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const status = exception.getStatus();
     const responseBody: any = exception.getResponse();
 
-    if (status === 400 || status === 401) {
-      const errorResponse: any = {
-        errorsMessages: [],
-        timestamp: new Date().toISOString(),
-        path: request.url,
-      };
-
-      if (Array.isArray(responseBody.message)) {
-        responseBody.message.forEach((m) => {
-          const match = m.match(/^(.+?) /);
-          const field = match ? match[1] : null;
-
-          errorResponse.errorsMessages.push({
-            message: m,
-            field: field,
-          });
-        });
-      }
-
-      if (errorResponse.errorsMessages.length === 0) {
-        response.status(status).json({
-          statusCode: status,
-          error: responseBody.message,
-          timestamp: new Date().toISOString(),
-          path: request.url,
-        });
-      }
-
-      response.status(status).json(errorResponse);
-    } else {
-      response.status(status).json({
-        statusCode: status,
-        timestamp: new Date().toISOString(),
-        path: request.url,
-      });
-    }
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
   }
 }
